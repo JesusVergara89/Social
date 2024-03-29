170baee8e139d04a0767a45ad70a541dfefa0c7e
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-03-29 12:39:05 -0500

    new feautures friendsship related

--- src/components/messages/Friendrequest.jsx
+++ src/components/messages/Friendrequest.jsx
@@ -1,39 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useParams } from 'react-router-dom'
 import { auth, db } from '../../firebaseConfig'
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, query } from 'firebase/firestore'
 import { toast } from 'react-toastify'
 import './Friendrequest.css'
 
 const Friendrequest = () => {
     const { id } = useParams()
     const [user] = useAuthState(auth)
+    const [pending, setPending] = useState([]);
+    const [myPending, setMyPending] = useState([])
+
+    ////////////////////////////////////////////////////////////////////
+    {/** Aqui solicito la db Reques a ver si ya son amigos para impedir que vuelvan 
+      a mandar solicitud de amistad*/}
+
+    useEffect(() => {
+        const usersCollectionRef = collection(db, 'Request');
+        const q = query(usersCollectionRef);
+        onSnapshot(q, (snapshot) => {
+            const requests = snapshot.docs.map((doc) => {
+                const { createdAt, ...dataWithoutCreatedAt } = doc.data();
+                return { id: doc.id, ...dataWithoutCreatedAt };
+            });
+            setPending(requests);
+        });
+    }, []);
+
+
+    const verifyMyPending = () => {
+        if (pending) {
+            const userPending = pending.filter(pen =>
+                pen.friendRequests.some(data => data.id1 === user.uid) &&
+                !pen.friendRequests.every(data => data.status === true) &&
+                !pen.friendRequests.every(data => data.status === false)
+            );
+            setMyPending(userPending.length > 0 ? userPending : null);
+        }
+    };
+
+    useEffect(() => {
+        verifyMyPending()
+    }, [pending])
+
+    ////////////////////////////////////////////////////////////////////
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            const postRef = collection(db, 'Request');
-            const newPost = {
-                friendRequests: [
-                    {
-                        createdAt: new Date(),
-                        id1: id,
-                        status: false
-                    },
-                    {
-                        createdAt: new Date(),
-                        id2: user.uid,
-                        status: true
-                    }
-                ]
-            }
-            await addDoc(postRef, newPost);
+        if (myPending) {
+            try {
+                const postRef = collection(db, 'Request');
+                const newPost = {
+                    friendRequests: [
+                        {
+                            createdAt: new Date(),
+                            id1: id,
+                            status: false
+                        },
+                        {
+                            createdAt: new Date(),
+                            id2: user.uid,
+                            status: true
+                        }
+                    ]
+                }
+                await addDoc(postRef, newPost);
 
-            toast('Request successfully', { type: 'success' });
-        } catch (error) {
-            console.error('Error request: ', error);
-            toast('Error request', { type: 'error' });
+                toast('Request successfully', { type: 'success' });
+            } catch (error) {
+                console.error('Error request: ', error);
+                toast('Error request', { type: 'error' });
+            }
+        }
+        else {
+            toast('ya eres amigo de esta persona o solicitud de amistad rechazada', { type: 'error' });
         }
     };
 
--- src/components/messages/Pendingfriendrequests.jsx
+++ src/components/messages/Pendingfriendrequests.jsx
@@ -29,9 +29,11 @@ const Pendingfriendrequests = () => {
     const verifyMyPending = () => {
         if (pending) {
             const userPending = pending.filter(pen =>
-                pen.friendRequests.some(data => data.id1 === user.uid)
+                pen.friendRequests.some(data => data.id1 === user.uid) &&
+                !pen.friendRequests.every(data => data.status === true) &&
+                !pen.friendRequests.every(data => data.status === false)
             );
-            setMyPending(userPending);
+            setMyPending(userPending.length > 0 ? userPending : null);
         }
     };
 
@@ -39,10 +41,8 @@ const Pendingfriendrequests = () => {
         verifyMyPending()
     }, [pending])
 
-    console.log(myPending)
-
-    const handleAccept = () => {
-        if (myPending.length > 0) {
+    const handleAccept = (id) => {
+        if (myPending.length > 0 && id === 'x') {
             const friendRequests = myPending[0].friendRequests.map((request, index) => {
                 if (index === 0) {
                     return { ...request, status: true };
@@ -59,28 +59,52 @@ const Pendingfriendrequests = () => {
                 .catch((error) => {
                     console.error("Error al actualizar el documento:", error);
                 });
-        } else {
-            console.log("No hay solicitudes pendientes para aceptar.");
+        } else if (myPending.length > 0 && id === 'y') {
+            const friendRequests = myPending[0].friendRequests.map((request, index) => {
+                if (index === 1) {
+                    return { ...request, status: false };
+                }
+                return request;
+            });
+
+            const acceptRef = doc(db, "Request", myPending[0].id);
+
+            updateDoc(acceptRef, { friendRequests })
+                .then(() => {
+                    console.log("Documento actualizado exitosamente.");
+                })
+                .catch((error) => {
+                    console.error("Error al actualizar el documento:", error);
+                });
         }
     };
 
     return (
-        <div className='Pendingfriendrequests'>
-            <h2>{`Hola ${user.displayName} tienes`}</h2>
-            <h2>{`${myPending && myPending.length} solicitud de amistad`}</h2>
-            {myPending && myPending.map((requestsArray, i) => (
-                <div key={i} className='card-Pendingfriendrequests'>
-                    {requestsArray.friendRequests.map((request, j) => (
-                        <div key={j}>
-                            <h5>{request.id1}</h5>
-                            <h5>{request.id2}</h5>
-                            <h5>{request.status === true ? 'true' : 'false'}</h5>
-                        </div>
+        <>{myPending === null ?
+            <h2 className='no-request'>No tienes solicitudes</h2>
+            :
+            (
+                <div className='Pendingfriendrequests'>
+                    <h2>{`Hola ${user.displayName} tienes`}</h2>
+                    <h2>{`${myPending && myPending.length} solicitud de amistad`}</h2>
+                    {myPending && myPending.map((requestsArray, i) => (
+                        <>
+                            <div key={i} className='card-Pendingfriendrequests'>
+                                {requestsArray.friendRequests.map((request, j) => (
+                                    <div key={j}>
+                                        <h5>{request.id1}</h5>
+                                        <h5>{request.id2}</h5>
+                                        <h5>{request.status === true ? 'true' : 'false'}</h5>
+                                    </div>
+                                ))}
+                            </div>
+                            <button onClick={() => handleAccept('x')}>acceptar</button>
+                            <button onClick={() => handleAccept('y')}>rechazar</button>
+                        </>
                     ))}
                 </div>
-            ))}
-            <button onClick={handleAccept}>update</button>
-        </div>
+            )}
+        </>
     )
 }
 
