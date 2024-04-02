28a74abb91ce336129e55ba0c593baa79b6c9b2b
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-02 16:15:35 -0500

    adding counters for the propose of stats

--- src/counters/Friendcounter.jsx
+++ src/counters/Friendcounter.jsx
@@ -2,11 +2,13 @@ import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { auth, db } from '../firebaseConfig';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import './Friendcounter.css'
 
 const Friendcounter = () => {
 
     const [friends, setFriends] = useState()
     const [user] = useAuthState(auth)
+    {/*const [matchFriends, setmatchFriends] = useState(second)*/ }
 
     useEffect(() => {
         const messagesRef = collection(db, 'Request')
@@ -20,21 +22,29 @@ const Friendcounter = () => {
         })
     }, []);
 
-    const matchFriends = friends.filter(data => {
-        for (let i = 0; i < data.friendRequests.length; i++) {
-            if ((data.friendRequests[i].id1 === user.uid || data.friendRequests[i].id2 === user.uid) &&
-                data.friendRequests[i].status === true && data.friendRequests[i + 1].status === true) {
-                return data;
-            }
-        }
-        return false;
-    });
+    let matchFriends = []
 
-    console.log(matchFriends)
+    if (friends) {
+        matchFriends = friends.filter(data => {
+            for (let i = 0; i < data.friendRequests.length; i++) {
+                if ((data.friendRequests[i].id1 === user.uid || data.friendRequests[i].id2 === user.uid) &&
+                    data.friendRequests[i].status === true && data.friendRequests[i + 1].status === true) {
+                    return data
+                }
+            }
+            return false;
+        });
+    }
+    //console.log(matchFriends)
 
     return (
-        <div>
-            {matchFriends.length}
+        <div className='Friendcounter'>
+            <div className="Friendcounter-number">
+                <h4><span>{matchFriends.length}</span></h4>
+            </div>
+            <div className="Friendcounter-connections">
+                <h4>Conexiones</h4>
+            </div>
         </div>
     )
 }
