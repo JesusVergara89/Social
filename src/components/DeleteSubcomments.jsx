ee8eac032c6d611ddc2d92fe6509221e4095c9be
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-08 22:03:50 -0500

    delete subcomments ready

--- src/components/DeleteSubcomments.jsx
+++ src/components/DeleteSubcomments.jsx
@@ -4,47 +4,53 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebaseConfig'
 import { doc, getDoc, updateDoc } from 'firebase/firestore'
 
-const DeleteSubcomments = ({ post, sub, indexSub }) => {
+const DeleteSubcomments = ({ post, sub, subcommentsFormatted, indexSub }) => {
 
     const [currentoLogUser] = useAuthState(auth)
 
-    const handleDeletePost = async () => {
+    const handleDeletePost = async (data) => {
         try {
             const docRef = doc(db, 'Post', post.id);
             const docSnap = await getDoc(docRef);
-            if (docSnap.exists()) {
-                let commentsData = docSnap.data().comments;
-                if (commentsData) {
-                    let claves = ['one', 'two', 'three', 'four'];
-                    if (commentsData[0].others && claves[indexSub]) {
-                        let subcomentario = commentsData[0].others[claves[indexSub]];
-                        subcomentario.content = "";
-                        subcomentario.createdAt = null;
-                        subcomentario.userID = "";
-                        subcomentario.userName = "";
-                        await updateDoc(docRef, { comments: commentsData });
-                        console.log('Subcomentario eliminado exitosamente');
-                    } else {
-                        console.log('El subcomentario no existe');
+                if (docSnap.exists()) {
+                    let commentsData = docSnap.data().comments;
+                    let keyFound = null;
+                    for (let clave in commentsData[0].others) {
+                        let data1 = commentsData[0].others[clave];
+                        if (data1.content == data.content &&
+                            data1.userID == data.userID) {
+                                keyFound = clave;
+                            break;
+                        }
                     }
-                } else {
-                    console.log('No se encontraron comentarios');
+                    if (keyFound) {
+                        delete commentsData[0].others[keyFound];
+                        let emptyObject = {
+                            content: "",
+                            createdAt: null,
+                            userID: "",
+                            userName: ""
+                        };
+                        commentsData[0].others[keyFound] = emptyObject;
+                        await updateDoc(docRef, { comments: commentsData });
+                        console.log(`subcomentario ${keyFound} eliminado`);
+                    } else {
+                        console.log('No se encontró el subcomentario a eliminar');
+                    }
                 }
-            } else {
-                console.log('No existe el documento');
-            }
         } catch (error) {
             console.log('Error obteniendo documento:', error);
         }
     };
 
-    //console.log(indexSub)
+
+    //console.log(subcommentsFormatted)
 
     return (
         <>
-            {currentoLogUser?.uid === sub.userID ?
+            {currentoLogUser?.uid === subcommentsFormatted[indexSub].userID ?
                 <div className="delete-btn-2">
-                    <button onClick={handleDeletePost}><i className='bx bxs-x-circle'></i></button>
+                    <button onClick={() => handleDeletePost(subcommentsFormatted[indexSub])}><i className='bx bxs-x-circle'></i></button>
                 </div>
                 :
                 ''
---
+++ src/components/tast.js
@@ -0,0 +1,21 @@
+
+others = {
+    one : {
+        content: '',
+        createdAt: null,
+        userID: ''
+    },
+    two : {
+        content: '',
+        createdAt: null,
+        userID: ''
+    },
+    three : {
+        content: 'jesus',
+        createdAt: 'bla',
+        userID: '1069480198'
+    }
+}
+
+
+
