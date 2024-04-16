64cf9d54c76144849d8ba489bf111624e97c9cc9
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-16 13:27:46 -0500

    sms logic finished

--- src/components/messages/Chat/Cardmsg.jsx
+++ src/components/messages/Chat/Cardmsg.jsx
@@ -9,6 +9,8 @@ const Cardmsg = () => {
 
     const [user] = useAuthState(auth);
     const [allmsg, setAllmsg] = useState();
+    const [timer, setTimer] = useState([])
+    const [msjNotification, setMsjNotification] = useState([])
 
     useEffect(() => {
         const querySnapshot = collection(db, 'Messages');
@@ -19,6 +21,15 @@ const Cardmsg = () => {
                 ...doc.data()
             }))
             setAllmsg(msgs)
+        })
+        const timerRef = collection(db, 'timerMsg');
+        const q1 = query(timerRef, orderBy('data'))
+        onSnapshot(q1, (snapshot) => {
+            const timers = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setTimer(timers)
         })
     }, []);
 
@@ -33,12 +44,45 @@ const Cardmsg = () => {
         return false;
     });
 
+    useEffect(() => {
+        if (user) {
+            const newMsjNotification = [];
+            for (const obj of timer) {
+                const filteredData = obj?.data?.filter(item =>
+                    item.creatorID === user.uid || item.receptorID === user.uid
+                );
+                if (filteredData.length > 0) {
+                    const lastObject = filteredData[filteredData.length - 1];
+                    if (lastObject.receptorID === user.uid && lastObject.userNameR !== '' && lastObject.userNameS !== '') {
+                        newMsjNotification.push(lastObject);
+                    }
+                }
+            }
+            setMsjNotification(newMsjNotification);
+        }
+    }, [timer, user, setMsjNotification]);
+
+    //console.log(msjNotification);
+
     return (
         <div className="card-msg">
+            <h3 className={msjNotification.length > 0 ? 'card-msg-title' : 'card-msg-title-none'}>Mensajes recientes:</h3>
+            <div className={msjNotification.length > 0 ? 'card-msg-newsms' : 'card-msg-newsms-none'}>
+                {msjNotification.length > 0 ?
+                    msjNotification.map((data, i) => (
+                        <div key={i} className="card-msg-newsms-sms">
+                            <h4>Tienes mensajes nuevos de:</h4>
+                            <h4><span>@{data.userNameS}</span></h4>
+                        </div>
+                    ))
+                    :
+                    ''
+                }
+            </div>
             <h3 className='card-msg-title'>tus mensajes con otros usuarios:</h3>
             {userMsgs && userMsgs.map((msg, i) => (
                 <Link key={i + 1} to={msg.message[0].receptor === user.uid ? `/Sendmessage/${msg.message[0].sender}/${user.uid}` : `/Sendmessage/${msg.message[0].receptor}/${user.uid}`}>
-                    <div  key={i} className='card-msg-info'>
+                    <div key={i} className='card-msg-info'>
                         <div className="card-user1">
                             <img src={msg.message[0].photoR} alt="" />
                             <h4>{`@${msg.message[0].userNameR}`}</h4>
