a4ca76cdc5080c76e0f2f252421271d5033a8eb0
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-20 19:28:38 -0500

    new branch multiplepost and modify file

--- src/components/Createmultipost.jsx
+++ src/components/Createmultipost.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Createmultipost = () => {
   return (
-    <div>Createmultipost</div>
+    <div className='Createmultipost' >
+        <h1>
+            here im goint to create a new way to post, multiple post
+        </h1>
+    </div>
   )
 }
 
