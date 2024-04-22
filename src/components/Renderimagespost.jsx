7d12c83cb9f29bea5cc3ccb444c43daf92d2f5d7
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-04-21 23:00:41 -0500

    working on styles

--- src/components/Renderimagespost.jsx
+++ src/components/Renderimagespost.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../style/Renderimagespost.css'
 
 const Renderimagespost = ({ id, images }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -11,19 +12,21 @@ const Renderimagespost = ({ id, images }) => {
     return (
         <div className='Renderimagespost'>
             <div className="carousel">
-                <div className="slides">
-                    {images.map((image, index) => (
-                        <div key={index} className={index === currentImageIndex ? "slide active" : "slide"}>
-                            <Link to={`/singlepost/${id}`}>
+                <Link to={`/singlepost/${id}`}>
+                    <div className="slides">
+                        {images.map((image, index) => (
+                            <div key={index} className={index === currentImageIndex ? "slide active" : "slide"}>
+
                                 <img src={image} alt={`Slide ${index}`} />
-                            </Link>
-                        </div>
-                    ))}
-                </div>
+
+                            </div>
+                        ))}
+                    </div>
+                </Link>
                 <div className="buttons-container">
                     {images.map((_, index) => (
                         <button key={index} onClick={() => goToSlide(index)} className={index === currentImageIndex ? "active" : ""}>
-                            <div className="circulo"></div>
+                            <div className={index === currentImageIndex ? "circulo active": "circulo" }></div>
                         </button>
                     ))}
                 </div>
