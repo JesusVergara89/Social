556785427dd0eff5ad0340452a0ddbd952683fd2
Brian Escorcia <naziegonzalez5@gmail.com>
2024-05-17 12:09:58 -0500

    update of comment layout and image size in messages

--- src/components/messages/Chat/Emojireactionlarger.jsx
+++ src/components/messages/Chat/Emojireactionlarger.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import './Emojireactionlarger.css'
 
 const Emojireactionlarger = ({ reaction }) => {
+    console.log(reaction)
     return (
-        <div className="display-chat-img-larger-reactions">
-            {
-                reaction.map((data, i) => (
-                    <div key={i}>
-                        <i className='span1'>
-                            {data.emoji}
-                        </i>
-                        <p className='span2'>
-                            @{data.userName}
-                        </p>
-                    </div>
-                ))
-            }
-        </div>
+        <>
+            {reaction?.[0] &&
+                <div className="display-chat-img-larger-reactions">
+                    {
+                        reaction.map((data, i) => (
+                            <div key={i}>
+                                <i className='span1'>
+                                    {data.emoji}
+                                </i>
+                                <p className='span2'>
+                                    @{data.userName}
+                                </p>
+                            </div>
+                        ))
+                    }
+                </div>}
+        </>
     )
 }
 
